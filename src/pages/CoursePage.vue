11ba3236a60dd76a1ae3527ccd33eb8e07ad83cd
symant233 <symant233@gmail.com>
2022-05-18 23:41:30 +0800

    update courses page

--- src/pages/CoursePage.vue
+++ src/pages/CoursePage.vue
@@ -2,12 +2,35 @@
   <div>
     <h1 class="font-weight-light m-title">课程</h1>
     <v-divider style="margin-bottom: 1rem"></v-divider>
-    <div class="m-container">
-      <div v-for="(item, index) in courses" :key="index" class="m-content">
-        <img :src="item.image" class="u-image" />
-        <div>{{ item.name }}</div>
-      </div>
-    </div>
+    <v-row>
+      <v-col
+        v-for="(item, index) in courses"
+        :key="index"
+        class="d-flex child-flex"
+        cols="3"
+      >
+        <div class="m-content">
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${index * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${index * 5 + 10}`"
+            aspect-ratio="1"
+            class="grey lighten-2"
+            cover
+            width="100%"
+          >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+          <p class="m-text">{{ item.name }}<br />{{ item.en }}</p>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -15,30 +38,31 @@
 export default {
   data: () => ({
     courses: [
-      { name: "book", image: require("@/assets/ct-lab-book.jpg") },
-      { name: "equip", image: require("@/assets/ct-lab-equip.jpg") },
+      { name: "嵌入式系统", en: "Embedded System" },
+      { name: "高级计算机体系结构", en: "Advanced Computer Architecture" },
+      { name: "计算机体系结构", en: "Computer Architecture" },
+      { name: "计算机组成", en: "Computer Organization" },
     ],
   }),
 };
 </script>
 
 <style scoped>
-.m-container {
-  display: flex;
-  gap: 1rem;
-  width: 100%;
-  height: 100%;
-}
 .m-content {
-  flex: 0 0 auto;
+  width: 100%;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%);
 }
 .m-title {
   text-align: center;
   margin-bottom: 16px;
 }
-.u-image {
-  height: 20rem;
-  object-fit: cover;
+.m-text {
+  padding: 0.5rem;
+}
+@media screen and (max-width: 600px) {
+  .v-col-3 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
 }
 </style>
